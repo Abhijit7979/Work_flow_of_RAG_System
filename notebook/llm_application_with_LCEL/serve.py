8149bef55b711fcba9153be4155f9b78bd2c9406
Abhijit7979 <abhijitrao79@gmail.com>
2025-08-21 23:06:51 +0530

    Worked on chat context

--- notebook/llm_application_with_LCEL/serve.py
+++ notebook/llm_application_with_LCEL/serve.py
@@ -1,3 +1,5 @@
+
+
 from fastapi import FastAPI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
@@ -26,16 +28,16 @@ parser=StrOutputParser()
 # Create chain
 chain=prompt_template|model|parser
 
-# # 2. ✅ Define Pydantic Input and Output models
-# class Input(BaseModel):
-#     language: str
-#     text: str
+# 2. ✅ Define Pydantic Input and Output models
+class Input(BaseModel):
+    language: str
+    text: str
 
-# class Output(BaseModel):
-#     output: str # The default output key for a single-output chain is "output"
+class Output(BaseModel):
+    output: str # The default output key for a single-output chain is "output"
 
-# # 3. ✅ Add types to your chain
-# chain = chain.with_types(input_type=Input, output_type=Output)
+# 3. ✅ Add types to your chain
+chain = chain.with_types(input_type=Input, output_type=Output)
 
 # App definition
 app=FastAPI(
@@ -47,10 +49,10 @@ app=FastAPI(
 # Adding chain routes
 add_routes(
     app,
-    chain,
+    chain, # Pass the typed chain
     path="/chain",
-    enabled_endpoints=["invoke", "stream", "playground"]
+    enabled_endpoints=["invoke", "stream", "playground","input_schema",'output_schema','config_schema','stream_log','stream_events']
 )
 
 if __name__=="__main__":
-    uvicorn.run(app,host="127.0.0.1",port=8014)
+    uvicorn.run(app,host="127.0.0.1",port=8017)
